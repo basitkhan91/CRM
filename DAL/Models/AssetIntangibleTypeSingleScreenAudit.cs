5c6e126c76fea36aabc14ea7270fbe9584300cea
Pas <poweraerosuites@gmail.com>
2019-05-02 10:18:32 +0530

    Commit for AuditModels

---
+++ DAL/Models/AssetIntangibleTypeSingleScreenAudit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Models
+{
+    public class AssetIntangibleTypeSingleScreenAudit : PasBase
+    {
+        [Key]
+        public long? AssetIntangibleTypeSingleAuditId { get; set; }
+        public long? AssetIntangibleTypeSingleId { get; set; }
+        public string AssetIntangibleSingleId { get; set; }
+        public string AssetIntangibleName { get; set; }
+        public Int32? MasterCompanyId { get; set; }
+        public string AssetIntangibleMemo { get; set; }
+        public bool IsDelete { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
