b8441713a6217283359a9e8159e1067c8f380dcb
Bharathi <poweraerosuites@gmail.com>
2019-05-04 21:21:01 +0530

    commit

---
+++ QuickApp.Pro/ClientApp/app/audit/audit.module.ts
@@ -0,0 +1,29 @@
+﻿
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AuditComponent } from "../components/Audit/audit.component";
+
+@NgModule({
+	declarations: [
+		AuditComponent
+    ],
+
+    imports: [
+        CommonModule
+    ],
+
+    exports: [
+        AuditComponent
+    ],
+
+    providers: [
+    ],
+
+    bootstrap: [
+        AuditComponent
+    ],
+
+})
+export class AuditModule {
+
+}
