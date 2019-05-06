58a5ad39a6d02a5da52bdf951c92adfe23fed177
Pas <poweraerosuites@gmail.com>
2019-05-06 13:41:00 +0530

    Model added for audit

---
+++ DAL/Models/AssetTypeSingleScreenAudit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DAL.Models
+{
+   public class AssetTypeSingleScreenAudit : PasBase
+    {
+        [Key]
+        public long? AssetTypeSingleScreenAuditId { get; set; }
+        public long? AssetTypeSingleScreenId { get; set; }
+        public string AssetTypeSingleId { get; set; }
+        public string AssetTypeName { get; set; }
+        public Int32? MasterCompanyId { get; set; }
+        public string AssetTypeMemo { get; set; }
+        public bool? IsDelete { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
