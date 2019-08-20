f71a89cd3fd155ef864571588f5125fb1f8252be
Sachin Thakkar <projectpas10@gmail.com>
2019-08-20 19:11:26 +0530

    IF Conditions

--- DAL/Repositories/ItemMasterRepository.cs
+++ DAL/Repositories/ItemMasterRepository.cs
@@ -545,36 +545,35 @@ namespace DAL.Repositories
                             where it.IsActive == true && it.ItemMasterId == ItemmasterId && myAircraftTypeId.Contains(it.AircraftTypeId) && myAircraftModelId.Contains(it.AircraftModelId) && myDashNumberId.Contains(it.DashNumberId) && it.IsDeleted!=true
                             select new{it.ItemMasterId,it.PartNumber,it.AircraftTypeId,it.AircraftModelId,it.DashNumberId,it.DashNumber,it.AircraftType,it.AircraftModel,it.Memo,it.MasterCompanyId,it.IsActive,it.IsDeleted}).ToList();
                             return data;
-            }
-            if (AircraftTypeId != null && AircraftModelId != null && myDashNumberId == null)
+            }else if (AircraftTypeId != null && AircraftModelId != null && myDashNumberId == null)
             {
                             var data = (from it in _appContext.ItemMasterAircraftMapping
                             where it.IsActive == true && it.ItemMasterId == ItemmasterId && myAircraftTypeId.Contains(it.AircraftTypeId) && myAircraftModelId.Contains(it.AircraftModelId) && it.IsDeleted != true
                             select new{it.ItemMasterId,it.PartNumber,it.AircraftTypeId,it.AircraftModelId,it.DashNumberId,it.DashNumber,it.AircraftType,it.AircraftModel,it.Memo,it.MasterCompanyId,it.IsActive,it.IsDeleted}).ToList();
                             return data;
             }
-            if (AircraftTypeId != null && myAircraftModelId == null && myDashNumberId == null)
+            else if (AircraftTypeId != null && myAircraftModelId == null && myDashNumberId == null)
             {
                             var data = (from it in _appContext.ItemMasterAircraftMapping
                             where it.IsActive == true && it.ItemMasterId == ItemmasterId && myAircraftTypeId.Contains(it.AircraftTypeId) && it.IsDeleted != true
                             select new{it.ItemMasterId,it.PartNumber,it.AircraftTypeId,it.AircraftModelId,it.DashNumberId,it.DashNumber,it.AircraftType,it.AircraftModel,it.Memo,it.MasterCompanyId,it.IsActive,it.IsDeleted}).ToList();
                             return data;
             }
-            if (AircraftTypeId != null && myAircraftModelId == null && myDashNumberId != null)
+            else if (AircraftTypeId != null && myAircraftModelId == null && myDashNumberId != null)
             {
                             var data = (from it in _appContext.ItemMasterAircraftMapping
                             where it.IsActive == true && it.ItemMasterId == ItemmasterId && myAircraftTypeId.Contains(it.AircraftTypeId) && myDashNumberId.Contains(it.DashNumberId) && it.IsDeleted != true
                             select new{it.ItemMasterId,it.PartNumber,it.AircraftTypeId,it.AircraftModelId,it.DashNumberId,it.DashNumber,it.AircraftType,it.AircraftModel,it.Memo,it.MasterCompanyId,it.IsActive,it.IsDeleted}).ToList();
                             return data;
             }
-            if (AircraftTypeId == null && AircraftModelId != null && myDashNumberId != null)
+            else if (AircraftTypeId == null && AircraftModelId != null && myDashNumberId != null)
             {
                             var data = (from it in _appContext.ItemMasterAircraftMapping
                             where it.IsActive == true && it.ItemMasterId == ItemmasterId && myAircraftModelId.Contains(it.AircraftModelId) && myDashNumberId.Contains(it.DashNumberId) && it.IsDeleted != true
                             select new{it.ItemMasterId,it.PartNumber,it.AircraftTypeId,it.AircraftModelId,it.DashNumberId,it.DashNumber,it.AircraftType,it.AircraftModel,it.Memo,it.MasterCompanyId,it.IsActive,it.IsDeleted}).ToList();
                             return data;
             }
-            if (AircraftTypeId == null && myAircraftModelId == null && myDashNumberId != null)
+            else if (AircraftTypeId == null && myAircraftModelId == null && myDashNumberId != null)
             {
                             var data = (from it in _appContext.ItemMasterAircraftMapping
                             where it.IsActive == true && it.ItemMasterId == ItemmasterId && myDashNumberId.Contains(it.DashNumberId) && it.IsDeleted != true
@@ -603,14 +602,14 @@ namespace DAL.Repositories
                             select new{iM.ItemMasterATAMappingId,iM.ItemMasterId,iM.ATAChapterId,iM.ATAChapterCode,iM.ATAChapterName,iM.PartNumber,iM.ATASubChapterId,iM.ATASubChapterDescription}).ToList();
                             return data;
             }
-            if (ATAChapterId != null && ATASubChapterID == null)
+            else if (ATAChapterId != null && ATASubChapterID == null)
             {
                             var data = (from iM in _appContext.ItemMasterATAMapping
                             where iM.ItemMasterId == ItemMasterid && myATAChapterId.Contains(iM.ATAChapterId)  && iM.IsActive == true && iM.IsDeleted != true
                             select new { iM.ItemMasterATAMappingId, iM.ItemMasterId, iM.ATAChapterId, iM.ATAChapterCode, iM.ATAChapterName, iM.PartNumber, iM.ATASubChapterId, iM.ATASubChapterDescription }).ToList();
                             return data;
             }
-            if (ATAChapterId == null && ATASubChapterID != null)
+            else if (ATAChapterId == null && ATASubChapterID != null)
             {
                             var data = (from iM in _appContext.ItemMasterATAMapping
                             where iM.ItemMasterId == ItemMasterid && myATASubChapterID.Contains(iM.ATASubChapterId) && iM.IsActive == true && iM.IsDeleted != true
