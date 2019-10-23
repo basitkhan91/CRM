17da18673560aab2edd83f9d84e2f4f6c6bbf210
vamsisama <vamsi@silverxis.com>
2019-10-23 15:10:37 +0530

    changes done for the EMployee Module

--- QuickApp.Pro/ClientApp/app/components/employee/employee-training/employee-training.component.ts
+++ QuickApp.Pro/ClientApp/app/components/employee/employee-training/employee-training.component.ts
@@ -30,6 +30,7 @@ import { LocalStoreManager } from '../../../services/local-store-manager.service
 import { DBkeys } from '../../../services/db-Keys';
 
 import { User } from '../../../models/user.model';
+import { ItemMasterService } from '../../../services/itemMaster.service';
 
 
 
@@ -46,6 +47,7 @@ export class EmployeeTrainingComponent implements OnInit, AfterViewInit {
 	alltrainingTypes: any[];
 	allPurchaseUnitOfMeasureinfo: any[];
 	localunit: any[];
+    manufacturerData: any;
 
 	ngOnInit(): void {
 		this.employeeService.currentUrl = '/employeesmodule/employeepages/app-employee-training';
@@ -57,7 +59,8 @@ export class EmployeeTrainingComponent implements OnInit, AfterViewInit {
 			
         }
 		this.loadTariningTypes();
-		this.Purchaseunitofmeasure();
+        this.Purchaseunitofmeasure();
+        this.getAllAircraftManfacturer();
     }
     @ViewChild(MatPaginator) paginator: MatPaginator;
     @ViewChild(MatSort) sort: MatSort;
@@ -97,7 +100,7 @@ export class EmployeeTrainingComponent implements OnInit, AfterViewInit {
     private isEditMode: boolean = false;
     private isDeleteMode: boolean = false;
     Active: string = "Active";
-    constructor(private route: ActivatedRoute, private translationService: AppTranslationService, public unitService: UnitOfMeasureService, public authService: AuthService, private modalService: NgbModal, private activeModal: NgbActiveModal, private _fb: FormBuilder, private alertService: AlertService, private router: Router, public employeeService: EmployeeService, private dialog: MatDialog, private masterComapnyService: MasterComapnyService, private localStorage: LocalStoreManager,) {
+    constructor(private route: ActivatedRoute, private itemser: ItemMasterService, private translationService: AppTranslationService, public unitService: UnitOfMeasureService, public authService: AuthService, private modalService: NgbModal, private activeModal: NgbActiveModal, private _fb: FormBuilder, private alertService: AlertService, private router: Router, public employeeService: EmployeeService, private dialog: MatDialog, private masterComapnyService: MasterComapnyService, private localStorage: LocalStoreManager,) {
         this.displayedColumns.push('action');
 		this.dataSource = new MatTableDataSource();
 		if (this.employeeService.generalCollection) {
@@ -187,7 +190,17 @@ export class EmployeeTrainingComponent implements OnInit, AfterViewInit {
 			results => this.onTariningTypesData(results[0]),
 			error => this.onDataLoadFailed(error)
 		);
-	}
+    }
+    
+    getAllAircraftManfacturer() {
+        this.itemser.getAircraft().subscribe(res => {
+            this.manufacturerData = res[0].map(x => {
+                return {
+                    value: x.aircraftTypeId, label: x.description
+                }
+            })
+        });
+    }
 
     private loadMasterCompanies() {
         this.alertService.startLoadingMessage();
