c6fa234d791a3096663ddd614fa711e56ea81f82
Vamsi-Laptop <vamsi@silverxis.com>
2019-11-04 12:58:12 +0530

    changes done with the workOrderList

--- QuickApp.Pro/ClientApp/app/components/work-order/work-order-list/work-order-list.component.ts
+++ QuickApp.Pro/ClientApp/app/components/work-order/work-order-list/work-order-list.component.ts
@@ -1,8 +1,11 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit, ViewChild } from '@angular/core';
 import { fadeInOut } from '../../../services/animations';
 import { PageHeaderComponent } from '../../../shared/page-header.component';
 import * as $ from 'jquery';
 import { WorkOrderService } from '../../../services/work-order/work-order.service';
+import { Table } from 'primeng/table';
+import { AuthService } from '../../../services/auth.service';
+import { AlertService, MessageSeverity } from '../../../services/alert.service';
 
 @Component({
     selector: 'app-work-order-list',
@@ -13,17 +16,93 @@ import { WorkOrderService } from '../../../services/work-order/work-order.servic
 /** WorkOrderList component*/
 export class WorkOrderListComponent implements OnInit {
     /** WorkOrderList ctor */
-    workOrderList : any;
-    constructor(    private workOrderService: WorkOrderService,) {
+    workOrderData : any;
+    pageSize : number = 10;
+    @ViewChild('dt')
+    private table: Table;
+    lazyLoadEventData: any;
+    headers = [
+        // { field: 'workOrderNum', header: 'WorkOrder Number' },
+        { field : 'name' , header: 'Customer Name'},
+        { field: 'customerCode', header: 'customerCode' },
+        { field: 'workOrderType', header: 'WorkOrder Type' },
+        { field: 'openDate', header: 'Open Date' },
+        { field: 'workOrderStatus', header: 'WorkOrder Status' },
+    ]
+    selectedColumns = this.headers;
+    pageIndex: number = 0;
+    totalRecords: any;
+    totalPages: number;
+    constructor(    private workOrderService: WorkOrderService,     
+         private authService: AuthService,
+         private alertService: AlertService
+         ) {
 
     }
     ngOnInit(){
-    
+        // this.getAllWorkOrderList()
     }
 
-    getAllWorkOrderList(){
-        this.workOrderService.getWorkOrderList(0,10).subscribe(res => {
-            this.workOrderList = res;
+    get userName(): string {
+        return this.authService.currentUser ? this.authService.currentUser.userName : "";
+    }
+
+
+    columnsChanges() {
+        this.refreshList();
+    }
+    refreshList() {
+        this.table.reset();
+        // this.getList();
+        // this.table.sortOrder = 0;
+        // this.table.sortField = '';
+
+
+    }
+
+    loadData(event) {
+        this.lazyLoadEventData = event;
+        const pageIndex = parseInt(event.first) / event.rows;;
+        this.pageIndex = pageIndex;
+        this.pageSize = event.rows;
+        event.first = pageIndex;
+        this.getAllWorkOrderList(event)
+    }
+
+    getAllWorkOrderList(data){
+        this.workOrderService.getWorkOrderList( this.pageIndex , this.pageSize).subscribe(res => {
+            this.workOrderData = res;
+            if (res.length > 0) {
+                this.totalRecords = res[0].totalRecords;
+                this.totalPages = Math.ceil(this.totalRecords / this.pageSize);
+            }
+
+        })
+    }
+
+    globalSearch(value) {
+        // this.pageIndex = 0;
+        // this.workOrderService.getGlobalSearch(value, this.pageIndex, this.pageSize).subscribe(res => {
+        //     this.data = res;
+        //     if (res.length > 0) {
+        //         this.totalRecords = res[0].totalRecords;
+        //         this.totalPages = Math.ceil(this.totalRecords / this.pageSize);
+        //     }
+        // })
+    }
+
+    changeStatus(rowData) {
+        this.workOrderService.updateActionforWorkOrder(rowData, this.userName).subscribe(res => {
+            this.alertService.showMessage("Success", `Successfully Updated Status`, MessageSeverity.success);
+        })
+
+    }
+
+    delete(rowData) {
+        this.workOrderService.deleteActionforWorkOrder(rowData.workOrderId).subscribe(res => {
+            this.getAllWorkOrderList(this.lazyLoadEventData);
+            this.alertService.showMessage("Success", `Successfully Deleted Record`, MessageSeverity.success);
+
         })
     }
 }
