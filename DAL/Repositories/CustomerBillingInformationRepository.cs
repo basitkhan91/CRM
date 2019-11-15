5f89b6b708123ddfe44a40f3988d665b106820ee
Vijay <vreddy@silverxis.com>
2019-11-15 18:07:38 +0530

    Added CustomerBillingAddressAudit and CustomerShippingAddressAudit New API and realted services

--- DAL/Repositories/CustomerBillingInformationRepository.cs
+++ DAL/Repositories/CustomerBillingInformationRepository.cs
@@ -1,18 +1,16 @@
-﻿using System;
+﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using DAL.Models;
-using DAL.Repositories.Interfaces;
 
 namespace DAL.Repositories.Interfaces
 {
     class CustomerBillingInformationRepository : Repository<CustomerBillingAddress>, ICustomerBillingInformationRepository
     {
         public CustomerBillingInformationRepository(ApplicationDbContext context) : base(context)
-        { }
+        {
+        }
 
         public IEnumerable<CustomerBillingAddress> GetAllCustomerBillingAddress()
         {
@@ -25,7 +23,7 @@ namespace DAL.Repositories.Interfaces
                         join ad in _appContext.Address on v.AddressId equals ad.AddressId
                         join c in _appContext.Countries on ad.Country equals Convert.ToString(c.countries_id) into adc
                         from c in adc.DefaultIfEmpty()
-                        where ((v.IsDelete== null || v.IsDelete == false) && (v.CustomerId == id))
+                        where ((v.IsDelete == null || v.IsDelete == false) && (v.CustomerId == id))
 
                         // select new { t, ad, vt }).ToList();
                         select new
@@ -46,7 +44,7 @@ namespace DAL.Repositories.Interfaces
                             v.IsActive,
                             v.IsDelete,
                             v.IsPrimary,
-                            Country= c.countries_name
+                            Country = c.countries_name
 
 
                         }).ToList();
@@ -57,31 +55,61 @@ namespace DAL.Repositories.Interfaces
         public IEnumerable<Object> GetAllCusBillingHistory(long id)
         {
             var data = (from v in _appContext.CustomerBillingAddress
-                join ad in _appContext.Address on v.AddressId equals ad.AddressId
+                        join ad in _appContext.Address on v.AddressId equals ad.AddressId
                         join c in _appContext.Countries on ad.Country equals Convert.ToString(c.countries_id) into adc
                         from c in adc.DefaultIfEmpty()
                         where v.CustomerId == id
 
-                select new
-                {
-                    Address1 = ad.Line1,
-                    Address2 = ad.Line2,
-                    Address3 = ad.Line3,
-                    ad.AddressId,
-                    //ad.Country,
-                    ad.PostalCode,
-                    ad.City,
-                    ad.StateOrProvince,
-                    v.SiteName,
-                    v.CustomerBillingAddressId,
-                    v.CreatedDate,
-                    v.UpdatedDate,
-                    v.CustomerId,
-                    v.IsActive,
-                    v.IsDelete,
-                    v.IsPrimary,
-                    Country = c.countries_name
-                }).ToList();
+                        select new
+                        {
+                            Address1 = ad.Line1,
+                            Address2 = ad.Line2,
+                            Address3 = ad.Line3,
+                            ad.AddressId,
+                            //ad.Country,
+                            ad.PostalCode,
+                            ad.City,
+                            ad.StateOrProvince,
+                            v.SiteName,
+                            v.CustomerBillingAddressId,
+                            v.CreatedDate,
+                            v.UpdatedDate,
+                            v.CustomerId,
+                            v.IsActive,
+                            v.IsDelete,
+                            v.IsPrimary,
+                            Country = c.countries_name
+                        }).ToList();
+            return data;
+        }
+
+        public IEnumerable<object> GetAllCustomerBillingAddressAudit(long customerId, long addressId)
+        {
+            var data = (from v in _appContext.CustomerBillingAddressAudit
+                        join ad in _appContext.Address on v.AddressId equals ad.AddressId
+                        join c in _appContext.Countries on ad.Country equals Convert.ToString(c.countries_id) into adc
+                        from c in adc.DefaultIfEmpty()
+                        where v.CustomerId == customerId && v.CustomerBillingAddressId == addressId
+                        select new
+                        {
+                            Address1 = ad.Line1,
+                            Address2 = ad.Line2,
+                            Address3 = ad.Line3,
+                            ad.AddressId,
+                            ad.PostalCode,
+                            ad.City,
+                            ad.StateOrProvince,
+                            v.AuditCustomerBillingAddressId,
+                            v.SiteName,
+                            v.CustomerBillingAddressId,
+                            v.CreatedDate,
+                            v.UpdatedDate,
+                            v.CustomerId,
+                            v.IsActive,
+                            v.IsDelete,
+                            v.IsPrimary,
+                            Country = c.countries_name
+                        }).OrderBy(c=>c.AuditCustomerBillingAddressId).ToList();
             return data;
         }
 
