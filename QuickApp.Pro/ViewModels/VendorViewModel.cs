d983f05d38f920073f76c42eff70ed9167b5ca5b
Vijay <vreddy@silverxis.com>
2019-12-09 18:04:48 +0530

    Updated Model

--- QuickApp.Pro/ViewModels/VendorViewModel.cs
+++ QuickApp.Pro/ViewModels/VendorViewModel.cs
@@ -63,7 +63,7 @@ namespace QuickApp.Pro.ViewModels
         public Nullable<bool> v1099RoyaltiesDefault { get; set; }
         public long? capabilityId { get; set; }
         public Nullable<bool> v1099OtherIncome { get; set; }
-        public string v1099OtherIncomeDefault { get; set; }
+        public Nullable<bool> v1099OtherIncomeDefault { get; set; }
         public Nullable<bool> v1099MedicalHealthPayments { get; set; }
         public Nullable<bool> v1099MedicalHealthPaymentsDefault { get; set; }
         public Nullable<bool> v1099NonEmployeeComp { get; set; }
