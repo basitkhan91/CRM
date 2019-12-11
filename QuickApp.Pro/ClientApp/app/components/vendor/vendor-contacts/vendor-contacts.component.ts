5adaebe7cd1f346a71facc7fda4f61a3451f43ee
projectpas <54223580+projectpas@users.noreply.github.com>
2019-12-11 15:16:32 +0530

    Revert "Vendor bugs for Mobile no validation"

--- QuickApp.Pro/ClientApp/app/components/vendor/vendor-contacts/vendor-contacts.component.ts
+++ QuickApp.Pro/ClientApp/app/components/vendor/vendor-contacts/vendor-contacts.component.ts
@@ -653,13 +653,4 @@ export class VendorContactsComponent implements OnInit {
         this.sourceVendor = {};
     }
 
-    patternMobilevalidationWithSpl(event: any) {
-        const pattern = /[0-9\+\-()\ ]/;
-    
-        let inputChar = String.fromCharCode(event.charCode);
-        if (event.keyCode != 8 && !pattern.test(inputChar)) {
-          event.preventDefault();
-        }
-      }
-
 }
