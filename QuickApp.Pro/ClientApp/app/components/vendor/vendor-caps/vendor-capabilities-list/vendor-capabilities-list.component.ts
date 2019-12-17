4b6d394770e50bc01f35bfab17d6901fadbcefa7
suleman <suleman0498@gmail.com>
2019-12-17 19:37:48 +0530

    vendorcapes and fixes

--- QuickApp.Pro/ClientApp/app/components/vendor/vendor-caps/vendor-capabilities-list/vendor-capabilities-list.component.ts
+++ QuickApp.Pro/ClientApp/app/components/vendor/vendor-caps/vendor-capabilities-list/vendor-capabilities-list.component.ts
@@ -159,7 +159,7 @@ export class VendorCapabilitiesListComponent implements OnInit{
         this.isEditMode = false;
         this.isDeleteMode = true;
         this.sourceAction = row;
-        this.modal = this.modalService.open(content, { size: 'sm' });
+        this.modal = this.modalService.open(content, { size: 'sm', backdrop: 'static', keyboard: false });
         this.modal.result.then(() => {
             console.log('When user closes');
         }, () => { console.log('Backdrop click') })
@@ -272,7 +272,7 @@ export class VendorCapabilitiesListComponent implements OnInit{
 
         this.capabilityauditHisory = auditHistory;
 
-        this.modal = this.modalService.open(content, { size: 'lg' });
+        this.modal = this.modalService.open(content, { size: 'lg', backdrop: 'static', keyboard: false });
         this.modal.result.then(() => {
             console.log('When user closes');
         }, () => { console.log('Backdrop click') })
