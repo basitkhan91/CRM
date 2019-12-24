5f023b84d38a50356afac64fbd64a31c40c585a3
kishan530 <kishan.kish530@gmail.com>
2019-12-24 12:39:46 +0530

    sales quote UI issues and validaton issues

--- QuickApp.Pro/ClientApp/app/components/sales/shared/components/sales-part-number/sales-part-number.component.ts
+++ QuickApp.Pro/ClientApp/app/components/sales/shared/components/sales-part-number/sales-part-number.component.ts
@@ -18,6 +18,7 @@ export class SalesPartNumberComponent {
   part: PartDetail;
   selectedPart:IPartJson;
   selectedParts:any[] = [];
+  columns:any[];
   @ViewChild('addPart') addPart: ElementRef;
   @ViewChild('salesMargin') salesMargin: ElementRef;
   @Input() customer: any;
@@ -44,7 +45,46 @@ this.salesQuoteService.getSelectedParts()
 this.selectedParts = data;
 
 });
+this.columns = [];
+this.initColumns();
   
+}
+
+
+
+
+        
+
+
+
+
+
+initColumns() {
+  this.columns = [
+    { field: 'partNumber', header: 'PartNumber', width: '200px' },
+    { field: 'description', header: 'Description', width: '200px' },
+    { field: 'stockLineNumber', header: 'Stock Line Number', width: '200px' },
+    { field: 'method', header: 'Method', width: '200px' },
+    { field: 'serialNumber', header: 'Serial Number', width: '200px' },
+    { field: 'stockLineId', header: 'STOCK LINE NUM', width: '200px' },
+    { field: 'idNumber', header: 'ID NUM', width: '200px' },
+    { field: 'pmaStatus', header: 'OEM/PMA/DE', width: '200px' },
+    { field: 'conditionType', header: 'COND TYPE', width: '200px' },
+    { field: 'currency', header: 'Currency', width: '200px' },
+    { field: 'fixRate', header: 'fx Rate', width: '200px' },
+    { field: 'quantityFromThis', header: 'QTY QUOTED', width: '200px' },
+    { field: 'salesPricePerUnit', header: 'UNIT SALE PRICE', width: '200px' },
+    { field: 'markUpPercentage', header: 'MARK UP %', width: '200px' },
+    { field: 'markupExtended', header: 'MARK UP AMOUNT', width: '200px' },
+    { field: 'salesDiscount', header: 'DISCOUNT %', width: '200px' },
+    { field: 'salesDiscountPerUnit', header: 'DISC AMOUNT', width: '200px' },
+    { field: 'netSalesPriceExtended', header: 'NET SALES', width: '200px' },
+    { field: 'unitCostPerUnit', header: 'UNIT COST', width: '200px' },
+    { field: 'unitCostExtended', header: 'EXTENDED COST', width: '200px' },
+    { field: 'marginAmountExtended', header: 'MARGIN ($)', width: '200px' },
+    { field: 'marginPercentageExtended', header: 'MARGIN (%)', width: '200px' }
+
+  ]
 }
 
   onClose(event) {
@@ -56,6 +96,7 @@ this.selectedParts = data;
   
   onCloseMargin(event) {
     this.show = false;
+    this.selectedPart.selected = false;
     this.salesMarginModal.close();
     console.log('close event');
     this.openPartNumber();
@@ -95,6 +136,7 @@ this.selectedParts = data;
         this.salesQuoteService.getSearchPartObject()
         .subscribe(data => {
         this.query = data;
+        this.part = new PartDetail();
         this.part.partNumber = this.selectedPart.partNumber;
         this.part.stockLineNumber = this.selectedPart.stockLineNumber;
         this.part.salesPricePerUnit = + this.selectedPart.unitCost;
@@ -102,6 +144,7 @@ this.selectedParts = data;
         this.part.description = this.selectedPart.description;
         this.part.itemMasterId = this.selectedPart.itemId;
         this.part.stockLineId = this.selectedPart.itemId;
+        this.part.idNumber = this.selectedPart.idNumber;
         this.part.method = this.selectedPart.method;
         this.part.serialNumber = this.selectedPart.serialNumber;
         this.part.pmaStatus = 'OEM';
@@ -121,7 +164,7 @@ this.selectedParts = data;
         this.salesMarginModal = this.modalService.open(content, { size: 'lg' });
         this.salesMarginModal.result.then(() => {
             console.log('When user closes');
-        }, () => { console.log('Backdrop click') })
+        }, () => { console.log('Backdrop click'); this.selectedPart.selected = false; })
        }else{
          this.removePartNamber(this.selectedPart);
        }
@@ -131,7 +174,7 @@ this.selectedParts = data;
   }
   openSalesMarginSave(event){
     console.log(event);
-    if(!this.checkForDuplicates(event)){
+   // if(!this.checkForDuplicates(event)){
       this.salesQuoteService.getSearchPartObject()
       .subscribe(data => {
         this.query = data;
@@ -139,22 +182,47 @@ this.selectedParts = data;
         this.query.partSearchParamters.quantityToQuote = this.query.partSearchParamters.quantityRequested - this.query.partSearchParamters.quantityAlreadyQuoted;
       });
       this.salesQuoteService.updateSearchPartObject(this.query);
-      this.selectedParts.push(event);
+      let partObj = new PartDetail();
+      partObj.partNumber = this.part.partNumber;
+      partObj.stockLineNumber = this.part.stockLineNumber;
+      partObj.salesPricePerUnit = + this.part.salesPricePerUnit;
+      partObj.itemClassification = this.part.itemClassification;
+      partObj.description = this.part.description;
+      partObj.itemMasterId = this.part.itemMasterId;
+      partObj.stockLineId = this.part.stockLineId;
+      partObj.idNumber = this.part.idNumber;
+      partObj.method = this.part.method;
+      partObj.serialNumber = this.part.serialNumber;
+      partObj.pmaStatus =  this.part.pmaStatus;
+      partObj.masterCompanyId =  this.part.masterCompanyId;
+      partObj.conditionType =  this.part.conditionType;
+      partObj.currency =  this.part.currency;
+      partObj.salesDiscount = this.part.salesDiscount;
+      partObj.unitCostPerUnit = this.part.unitCostPerUnit;
+      partObj.markupPerUnit = this.part.markupPerUnit;
+      partObj.quantityRequested = this.part.quantityRequested;
+      partObj.quantityToBeQuoted = this.part.quantityToBeQuoted;
+      partObj.quantityFromThis = this.part.quantityFromThis;
+      partObj.quantityAlreadyQuoted =this.part.quantityAlreadyQuoted;
+      this.selectedParts.push(partObj);
       this.salesMarginModal.close();
       this.openPartNumber();
       console.log(this.query);
       console.log(this.selectedParts);
-    }
+   // }
    
   }
 
   checkForDuplicates(selectedPart){
     let selectedPartNamber = selectedPart.partNumber
     let selectedStockLineNumber = selectedPart.stockLineNumber
+    console.log(selectedPart);
+    console.log(this.selectedParts);
     for (let i = 0; i < this.selectedParts.length; i++) {
       let partNumber = this.selectedParts[i].partNumber;
       let stockLineNumber = this.selectedParts[i].stockLineNumber;
       console.log(partNumber);
+      console.log(stockLineNumber);
       if (partNumber==selectedPartNamber&&stockLineNumber==selectedStockLineNumber) {
         
         return true;
