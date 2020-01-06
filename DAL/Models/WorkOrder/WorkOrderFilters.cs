176b6841d6c4798463e2e0345e695d2149a2db3f
SureshA <SureshA@DESKTOP-VEQ7AVC>
2020-01-06 12:50:23 +0530

    WO List Changes

--- DAL/Models/WorkOrder/WorkOrderFilters.cs
+++ DAL/Models/WorkOrder/WorkOrderFilters.cs
@@ -9,16 +9,17 @@ namespace DAL.Models
         public long WorkOrderId { get; set; }
         public string WorkOrderNum { get; set; }
         public string CustomerName { get; set; }
-        public string CustomerCode { get; set; }
-        public string WorkOrderType { get; set; }
         public DateTime? OpenDate { get; set; }
         public string WorkOrderStatus { get; set; }
         public string PartNos { get; set; }
         public string WorkScope { get; set; }
         public string Priority { get; set; }
-        public string RevisedPN { get; set; }
         public string PNDescription { get; set; }
         public string Stage { get; set; }
-        
+        public string CustomerType { get; set; }
+        public DateTime? CustomerRequestDate {get;set;}
+        public DateTime? PromisedDate { get; set; }
+        public DateTime? EstimatedShipDate { get; set; }
+        public DateTime? EstimatedCompletionDate { get; set; }
     }
 }
