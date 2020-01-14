ed45b4c16e30b533df3c960d0ab9ff29b5818a21
SureshA <SureshA@DESKTOP-VEQ7AVC>
2020-01-14 12:53:09 +0530

    Item Master Capes changes

--- DAL/Models/Filters/ItemMasterCapesFilters.cs
+++ DAL/Models/Filters/ItemMasterCapesFilters.cs
@@ -10,16 +10,6 @@ namespace DAL.Models
         public long ItemMasterId { get; set; }
         public string partNo { get; set; }
         public string capabilityType { get; set; }
-        public string aircraftType { get; set; }
-        public string aircraftModel { get; set; }
-        public string aircraftDashNumber { get; set; }
-        //public long ManagementStructureId { get; set; }
-        public string description { get; set; }
-        public string aTAChapter { get; set; }
-        public string aTASubChapter { get; set; }
-        public DateTime? entryDate { get; set; }
-        public string cMM { get; set; }
-        public string integrateWith { get; set; }
         public bool? isVerified { get; set; }
         public string verifiedBy { get; set; }
         public DateTime? verifiedDate { get; set; }
