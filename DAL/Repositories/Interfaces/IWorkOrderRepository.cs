3384268d989d862ea4fd67272d325af183c1aede
SureshA <SureshA@DESKTOP-VEQ7AVC>
2020-01-17 18:27:41 +0530

    Global Settings Back End implementaion

--- DAL/Repositories/Interfaces/IWorkOrderRepository.cs
+++ DAL/Repositories/Interfaces/IWorkOrderRepository.cs
@@ -1,8 +1,6 @@
 ﻿using DAL.Common;
 using DAL.Models;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace DAL.Repositories.Interfaces
 {
---
+++ QuickApp.Pro/Controllers/GlobalSettingsController.cs
@@ -0,0 +1,83 @@
+﻿using DAL;
+using DAL.Common;
+using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuickApp.Pro.Controllers
+{
+    [Route("api/globalsettings")]
+    public class GlobalSettingsController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        public GlobalSettingsController(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        [HttpPost("createglobalsettings")]
+        public IActionResult CreateGlobalSettings([FromBody] GlobalSettings globalSetting)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var result = _unitOfWork.GlobalSettingsRepository.CreateGlobalSettings(globalSetting);
+                    return Ok(result);
+                }
+                else
+                {
+                    return BadRequest(ModelState.Values.FirstOrDefault().Errors);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("globalsettings")]
+        public IActionResult GetGlobalSettings(int masterCompanyId)
+        {
+            try
+            {
+                var result = _unitOfWork.GlobalSettingsRepository.GetGlobalSettings(masterCompanyId);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("getcultureinfos")]
+        public IActionResult GetCultureInfos()
+        {
+            try
+            {
+               var result= _unitOfWork.GlobalSettingsRepository.GetCultureInfos();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("globalsettingsinfo")]
+        public IActionResult GetGlobalSettingsInfo(string culture)
+        {
+            try
+            {
+                var result = _unitOfWork.GlobalSettingsRepository.GetGlobalSettingsInfo(culture);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
