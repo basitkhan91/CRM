6301aa50777f7fef31fae37bb6c789774c45d321
vamsisama <vamsi@silverxis.com>
2020-01-27 14:25:55 +0530

    Worked on Generic Methods and Customer Documents and Finance

--- QuickApp.Pro/ClientApp/app/services/customer-endpoint.service.ts
+++ QuickApp.Pro/ClientApp/app/services/customer-endpoint.service.ts
@@ -249,7 +249,7 @@ export class CustomerEndpoint extends EndpointFactory {
     }
 
     getDocumentList(customerId) {
-        return this.http.get(`${this.configurations.baseUrl}/api/Customer//getCustomerDocumentDetail/${customerId}`, this.getRequestHeaders())
+        return this.http.get(`${this.configurations.baseUrl}/api/Customer/getCustomerDocumentDetail/${customerId}`, this.getRequestHeaders())
     }
 
     postDomesticShipVia<T>(postData) {
