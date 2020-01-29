0c7a3eed180de0e71acebdaf392e2e564fdbd04a
venkat <Administrator@LENO-PF02TV9S>
2020-01-29 22:20:20 +0530

    Some more API for Legal Entity

---
+++ DAL/Repositories/Interfaces/ILegalEntityBillnformationRepository.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Models;
+using System.Collections.Generic;
+
+namespace DAL.Repositories.Interfaces
+{
+    public interface ILegalEntityBillnformationRepository : IRepository<LegalEntityBillingAddress>
+    {
+        IEnumerable<LegalEntityBillingAddress> GetAllLegalEntityBillingAddress();
+        IEnumerable<object> GetAllLegalEntityBillingDetails(long selectedrow);
+        IEnumerable<object> GetAllLegalEntityBillingHistory(long selectedrow);
+        IEnumerable<object> GetAllLegalEntityBillingAddressAudit(long LegalEntityId, long addressId);
+    }
+}
