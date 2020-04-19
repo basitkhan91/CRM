18a2f6c35ff81f143028842d3104d56aadabe001
Basit Khan <basitkhan91@outlook.com>
2020-04-20 05:09:01 +0530

    sprint
    login component added.

---
+++ CRMPortal/ClientApp/src/app/portal-login/portal-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'portal-login-component',
+    templateUrl: './portal-login-component.html',
+    styleUrls: ['./portal-login-component.scss']
+})
+export class PortalLoginComponent {
+
+}
